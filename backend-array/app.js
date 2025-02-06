cc0f21526b0bdf34c6003eb093b0c9c45cf5636c
Erik Silva <oliveiraerik464@gmail.com>
2025-02-05 21:43:15 -0300

    First Commit

---
+++ backend-array/app.js
@@ -0,0 +1,79 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+let fila = []
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send("Hello")
+});
+
+app.get('/queue', (req, res) => {
+  res.send({
+    'items': fila
+  })
+})
+
+app.post('/enqueue', (req, res) => {
+    const element = req.body.element;
+    fila.push(element)
+
+    res.send({
+      'element': element,
+      'pos': fila.length,
+    })
+})
+
+app.get('/enqueue/:element', function(req, res) {
+    const element = req.params.element
+    fila.push(element)
+    res.send({
+        'element': element,
+        'pos': fila.length,
+      })
+})
+
+app.get('/dequeue', function(req, res) {
+    const element = fila.length !== 0? fila.shift(): "A fila está vazia"
+    res.send({
+        'element': element,
+      })
+})
+
+app.get('/size', function(req, res) {
+    const size = lista.lenght
+    res.send({
+        'size': size,
+      })
+})
+
+app.get('/front', function(req, res) {
+    const front = fila.length === 0? "A fila está vazia": fila[0]
+    res.send({
+        'front': front,
+      })
+})
+
+app.get('/rear', function(req, res) {
+    const rear = fila.length === 0? "A fila está vazia": fila[fila.length-1]
+    res.send({
+        'rear': rear,
+      })
+})
+
+app.get('/isEmpty', function(req, res) {
+    const isEmpty = fila.length === 0? true: false
+    res.send({
+        'isEmpty': isEmpty,
+      })
+})
+
+app.listen(8000, () => {
+  console.log(`Server is running on port 8000 `);
+});
+
+export default app;
---
+++ backend-linked/app.js
@@ -0,0 +1,81 @@
+import express from 'express';
+import cors from 'cors';
+import {LinkedQueue} from "./linked.js"
+
+const app = express();
+const lQueue = new LinkedQueue()
+
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send("Hello")
+});
+
+app.get('/queue', (req, res) => {
+  res.send({
+    'items': lQueue
+  })
+})
+
+app.post('/enqueue', (req, res) => {
+    const element = req.body.element;
+    lQueue.enqueue(element)
+
+    res.send({
+      'element': element,
+      'pos': lQueue.size(),
+    })
+})
+
+app.get('/enqueue/:element', function(req, res) {
+    const element = req.params.element
+    lQueue.enqueue(element)
+    res.send({
+        'element': element,
+        'pos': lQueue.size(),
+      })
+})
+
+app.get('/dequeue', function(req, res) {
+    const element = lQueue.dequeue()
+    res.send({
+        'element': element,
+      })
+})
+
+app.get('/size', function(req, res) {
+    const size = lQueue.size()
+    res.send({
+        'size': size,
+      })
+})
+
+app.get('/front', function(req, res) {
+    const front = lQueue.front()
+    res.send({
+        'front': front,
+      })
+})
+
+app.get('/rear', function(req, res) {
+    const rear = lQueue.rear()
+    res.send({
+        'rear': rear,
+      })
+})
+
+app.get('/isEmpty', function(req, res) {
+    const isEmpty = lQueue.isEmpty()
+    res.send({
+        'isEmpty': isEmpty,
+      })
+})
+
+app.listen(8000, () => {
+  console.log(`Server is running on port 8000 `);
+});
+
+export default app;
